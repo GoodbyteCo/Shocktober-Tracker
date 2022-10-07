ceae5ea47bbc98e307e193474e1948ca0025c2aa
Quinn Pollock <contact@quinnpollock.net>
2022-10-06 23:40:42 -0700

    Revert "bad null handleing"
    This reverts commit 928677764433332207438a03bd3042c6c675e8aa.

--- src/components/Calender.vue
+++ src/components/Calender.vue
@@ -17,7 +17,7 @@
 			</h3>
 			<p
 				v-for="[username, filmList] in userFilmList.entries()"
-				:class="filmList.get(listToDisplay.get(calVal.date)?.name ?? '')?.toLowerCase() ?? '' + ' user'"
+				:class="filmList.get(listToDisplay.get(calVal.date)?.name ?? '')?.toLowerCase() + ' user'"
 			>
 				{{ username }}
 			</p>
