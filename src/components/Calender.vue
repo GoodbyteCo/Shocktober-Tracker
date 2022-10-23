ce28bdedf085d461a9ff8060ff1da06af25e4566
Jack Guinane <jackguinane@gmail.com>
2022-10-22 22:56:44 -0700

    Adjust formatting

--- src/components/Calender.vue
+++ src/components/Calender.vue
@@ -9,11 +9,13 @@
 				'padding': calVal.isPadding,
 				'date': true,
 			}"
-			:title="(filmExist(calVal.date)) ? listToDisplay.get(calVal.date)!.name : ''"
+			v-tippy="{
+				content: filmExist(calVal.date) ? listToDisplay.get(calVal.date)!.name : '',
+				followCursor: true,
+			}"
 			:href="(filmExist(calVal.date)) ? `https://letterboxd.com${listToDisplay.get(calVal.date)!.slug}` : ''"
 			target="_blank"
-			:content="(filmExist(calVal.date)) ? listToDisplay.get(calVal.date)!.name : ''"
-			v-tippy="{followCursor: true}">
+		>
 			<template v-if="!calVal.isPadding">
 				<h3>
 					{{ calVal.date + 1 }}
@@ -33,8 +35,6 @@
 <script setup lang="ts">
 	import type { Film, WatchStatus } from '@/types';
 	import { getDaysInMonth, firstDayInMonthIndex } from '@/utils'
-import { followCursor } from 'tippy.js';
-	import { tippy } from 'vue-tippy'
 	
 	type CalenderItem = {
 		isPadding: boolean
